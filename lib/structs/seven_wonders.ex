f42272e6991a6e91e7b919131507556cad0e74eb
AggelosAr <icesnowsword@gmail.com>
2025-11-10 19:03:51 +0200

    ADD structs examples:

---
+++ lib/structs/seven_wonders.ex
@@ -0,0 +1,32 @@
+defmodule Tutorials.Structs.SevenWonders do
+
+  defstruct name: "", country: ""
+
+  alias Tutorials.Structs.SevenWonders
+
+  @type t :: %SevenWonders{
+    name: String.t(),
+    country: String.t()
+  }
+
+
+  @spec all :: [t()]
+  def all() do
+    [
+      %SevenWonders{name: "Taj Mahal", country: "India"},
+      %SevenWonders{name: "Chichén Itzá", country: "Mexico"},
+      %SevenWonders{name: "Petra", country: "Jordan"},
+      %SevenWonders{name: "Machu Picchu", country: "Peru"},
+      %SevenWonders{name: "Christ the Redeemer", country: "Brazil"},
+      %SevenWonders{name: "Colosseum", country: "Italy"},
+      %SevenWonders{name: "The Great Wall of China", country: "China"}
+    ]
+  end
+
+  def print_names(wonders) do
+    Enum.each(wonders, fn x -> IO.puts(x.name) end)
+  end
+# Tutorials.Structs.SevenWonders.all |> Tutorials.Structs.SevenWonders.print_names
+# Tutorials.Structs.SevenWonders.print_names(Tutorials.Structs.SevenWonders.all())
+
+end
