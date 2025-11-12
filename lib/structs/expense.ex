16f11c4505c219fbc862cc917fa8710df578b56f
AggelosAr <icesnowsword@gmail.com>
2025-11-12 17:18:16 +0200

    ADD examples with expense samples

---
+++ lib/structs/expense.ex
@@ -0,0 +1,33 @@
+defmodule Tutorials.Structs.Expense do
+
+alias Tutorials.Structs.Expense
+
+  defstruct(
+    title: "",
+    date: nil,
+    amount: 0,
+    store: ""
+  )
+
+
+  # TODO what is the type t ?? and why all are of type t ?
+  @type t :: %Expense{
+        title: String.t(),
+        date: Date.t() | nil,
+        amount: number(),
+        store: String.t()
+        }
+
+
+  @spec sample() :: [t()]
+  def sample() do
+    [
+      %Expense{title: "Grocery", date: ~D[2022-09-12], amount: 18.99, store: "Metro"},
+      %Expense{title: "Mobile", date: ~D[2022-10-18], amount: 55.65, store: "Bell"},
+      %Expense{title: "Jacket", date: ~D[2022-07-06], amount: 150.99, store: "Marks"},
+      %Expense{title: "Air Jordans", date: ~D[2022-10-30], amount: 799.99, store: "Nike"}
+    ]
+  end
+
+
+end
